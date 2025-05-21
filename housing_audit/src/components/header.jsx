57a215a123b5b22233b813aa60282d385ab16732
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2025-05-21 13:28:50 +0200

    header almost done + added tailwindcss

---
+++ housing_audit/src/components/header.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+export default function Header() { 
+  return ( 
+    <div className="w-full border-b-2 border-black">
+      <div className="flex flex-row items-center mx-12">
+        <div>Logo</div>
+        <div className="flex flex-row items-center space-x-32 text-lg ml-auto text-palette-text font-medium">
+          <Link to="/">Home</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <button
+            className="bg-palette-red font-medium text-lg text-white px-6 py-1 ml-24 rounded-lg m-4">
+              Login
+        </button>
+      </div>  
+    </div>
+  );
+}
--- housing_audit/src/index.js
+++ housing_audit/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom'; 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/header'; 
+import Footer from './components/footer';
+import './globals.css';  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> 
+      <Header />  
+      <App /> 
+      <Footer />  
+    </Router>
   </React.StrictMode>
 );
 
