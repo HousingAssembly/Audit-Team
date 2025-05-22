e284d2c6165f98e14076ebfbd1f98dd6d2152b9d
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2025-05-22 11:05:01 +0200

    landing page

--- housing_audit/src/components/header.jsx
+++ housing_audit/src/components/header.jsx
@@ -1,15 +1,25 @@
 import { Link } from 'react-router-dom';
 
 export default function Header() { 
+
+  const scrollToSection = (e, sectionId) => {
+    e.preventDefault();    
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+      window.history.pushState({}, '', `#${sectionId}`);
+    }
+  };
+
   return ( 
-    <div className="w-full border-b-2 border-black">
+    <div className="w-full shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
       <div className="flex flex-row items-center mx-12">
         <img src="logo.png" alt="Logo" className="object-contain h-16 w-auto"/>
         <div className="px-6 text-4xl font-medium"><span className="text-palette-red">H</span>ouse <span className="text-palette-red">A</span>udit</div>
         <div className="flex flex-row items-center space-x-32 text-lg ml-auto text-palette-text font-medium">
           <Link to="/">Home</Link>
-          <Link to="#about">About Us</Link>
-          <Link to="#contact">Contact</Link>
+          <Link to="#about" onClick={(e) => scrollToSection(e, 'about')}>About Us</Link>
+          <Link to="#contact" onClick={(e) => scrollToSection(e, 'contact')}>Contact</Link>
         </div>
         <button className="bg-palette-red font-medium text-lg text-white px-6 py-1 ml-24 rounded-lg m-4">Login</button>
       </div> 
